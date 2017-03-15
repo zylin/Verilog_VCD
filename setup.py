c09603a2cc06ea8933a1d7930da4adecf04f3cce
Sameer Guaria <sgauria+python@gmail.com>
2017-03-15 07:47:41 +0100

    Import from source on pypi.python.org/Verilog_VCD/1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Verilog_VCD',
+    version='1.0',
+    author='Sameer Gauria',
+    author_email='sgauria+python@gmail.com',
+    packages=['Verilog_VCD'],
+    url='http://pypi.python.org/pypi/Verilog_VCD/',
+    description='Module to Parse VCD (Value-Change-Dump) files',
+    long_description=open('README.txt').read(),
+)
